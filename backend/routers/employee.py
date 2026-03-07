00fa41aeaf4ad3444b7f1c038f3b0d435dc0f262
Frostymyst <ethanwilson@shaw.ca>
2026-03-07 15:58:18 -0800

    adminSetEmployees
    Admin can now create and delete employees. Has been setup to be in admin.py router. Also removed these from employee.py router.

--- backend/routers/employee.py
+++ backend/routers/employee.py
@@ -36,31 +36,3 @@ async def search_employees(query: str):
             sql.close()
 
 
-@router.post("/")
-async def create_employee():
-    """Create a new employee"""
-    # TODO
-
-
-@router.get("/{employee_id}")
-async def get_employee(employee_id: int):
-    """Get employee by ID"""
-    # TODO
-
-
-@router.delete("/{employee_id}")
-async def delete_employee(employee_id: int):
-    """Delete employee by ID"""
-    # TODO
-
-
-@router.patch("/{employee_id}")
-async def update_employee(employee_id: int):
-    """Update employee by ID"""
-    # TODO
-
-
-@router.get("/departments")
-async def get_employee_departments():
-    """Get all employee departments"""
-    # TODO
