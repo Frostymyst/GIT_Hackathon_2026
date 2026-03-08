237948d23f83a77d691afffd4670890f3d890ea6
Talon Lusk <txlon5@student.ubc.ca>
2026-03-07 16:29:23 -0800

    Python job to fetch emails continuously

---
+++ backend/fetch_summarize.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+from services.fetch_summarize import run as fetch_emails_run
+
+# Force the script to load variables from the .env file in the /app directory
+load_dotenv()
+fetch_emails_run(True)
