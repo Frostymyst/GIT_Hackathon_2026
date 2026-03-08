23bde4abe2370ca13408b69ec70254592fb86987
Moop333 <Moop3334@gmail.com>
2026-03-07 17:48:25 -0800

    Fixed the search feature

--- front-end/src/components/MainList.jsx
+++ front-end/src/components/MainList.jsx
@@ -3,15 +3,17 @@ import './MainList.css'
 import Ticket from './Ticket';
 
 function MainList({ user, onNavigate, onLogout }) {
-    // eslint-disable-next-line no-unused-vars
-    let tickets;
+    let tickets; let ids;
     const req = new XMLHttpRequest();
     req.onload = () => {
         let taskResp = JSON.parse(req.responseText).tasks;
         tickets = taskResp.map((e) => <Ticket user={user} title={e.name} desc={e.summary} keywords={e.categories} id={e.tno}/>)
+        ids = taskResp.map((e) => <>
+        <option>{e.tno}</option><option>{e.name}</option>
+        </>)
     }
 
-    req.open("GET", "http://localhost:8000/task", false)
+    req.open("GET", "http://127.0.0.1:8000/task", false)
     req.send()
     /*req.setRequestHeader("Content-Type", "application/json")
     req.send(JSON.stringify({
@@ -26,13 +28,20 @@ function MainList({ user, onNavigate, onLogout }) {
         <tbody>
             <tr>
                 <td id='SearchDiv'>
-                    <h4>
-                        Search for a Specific Ticket
-                    </h4>
-                    <input type='text' name='search' id='TicketSearch' list='tickets' placeholder='Enter a Ticket Number'/> {/* Can also search by customer email if we have time */}
-                    <datalist id='tickets'>
-
-                    </datalist>
+                    <form role='search' onSubmit={(e) => {
+                        e.preventDefault()
+                        console.log(e.target.childNodes[1].value)
+                    }}>
+                        <label>
+                            <h4>
+                                Search for a Specific Ticket
+                            </h4>
+                        </label>
+                        <input type='search' name='search' id='TicketSearch' list='tickets' placeholder='Enter a Ticket Number'/> 
+                        <datalist id='tickets'>
+                            {ids}
+                        </datalist>
+                    </form>
                 </td>
                 <td id='TagDiv'>
                     <div id='Tags'>
